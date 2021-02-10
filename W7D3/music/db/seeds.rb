fcbb6773a9621a60519ca90eb1db672dd0a61a41
Ray <raysidarta@gmail.com>
2021-02-10 13:33:23 -0500

    W7D3 HW

---
+++ W7D3/music/db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+Band.create(name: "The Rolling Stones")
+Band.create(name: "The Beatles")
+Band.create(name: "Jo Mama")
+Band.create(name: "Thundercat")
+Band.create(name: "The Quintet of the Hotclub of France")
+Band.create(name: "Tatsuro Yamashita")
