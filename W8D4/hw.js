17eb8f56101f461580bdcfb372f272f5ab0efcc4
Ray <raysidarta@gmail.com>
2021-02-18 09:46:49 -0500

    Finish phase iv

--- W8D4/hw.js
+++ W8D4/hw.js
@@ -34,3 +34,43 @@ console.log(e.height);
 e.addTrick('Fights fire');
 console.log(e.tricks);
 console.log(e.play());
+console.log('\n\n\n');
+
+
+
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [e,ellie, charlie, kate, micah];
+
+
+
+function paradeHelper(elephant){
+    console.log(`${elephant.name} is trotting by!`);
+}
+herd.forEach(paradeHelper);
+
+
+
+
+
+function dinerBreakfast(food){
+    let foodList = ['cheesy scrambled eggs'];
+
+    printFoods = function(food){
+        foodList.push(food);
+        let sentence = foodList.join(' and ') + ' please';
+        console.log(sentence);
+    }
+    return printFoods;
+}
+
+let bfastOrder = dinerBreakfast();
+bfastOrder("chocolate chip pancakes");
+bfastOrder("grits");
+
+console.log(bfastOrder.foodList);
+
